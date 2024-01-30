e4712eea7387af7284dec894663abed70e244ab9
apella1 <johncollons5@gmail.com>
2024-01-30 16:54:01 +0300

    update: json user to match database user profile image field

--- utils/models.go
+++ utils/models.go
@@ -8,23 +8,25 @@ import (
 )
 
 type User struct {
-	ID        uuid.UUID `json:"id"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
-	FirstName string    `json:"first_name"`
-	LastName  string    `json:"last_name"`
-	Email     string    `json:"email"`
-	Password  string    `json:"password"`
+	ID           uuid.UUID `json:"id"`
+	CreatedAt    time.Time `json:"created_at"`
+	UpdatedAt    time.Time `json:"updated_at"`
+	FirstName    string    `json:"first_name"`
+	LastName     string    `json:"last_name"`
+	Email        string    `json:"email"`
+	Password     string    `json:"password"`
+	ProfileImage []byte    `json:"profile_image"`
 }
 
 func DatabaseUserToUser(dbUser database.User) User {
 	return User{
-		ID:        dbUser.ID,
-		CreatedAt: dbUser.CreatedAt,
-		UpdatedAt: dbUser.UpdatedAt,
-		FirstName: dbUser.FirstName,
-		LastName:  dbUser.LastName,
-		Email:     dbUser.Email,
-		Password:  dbUser.Password,
+		ID:           dbUser.ID,
+		CreatedAt:    dbUser.CreatedAt,
+		UpdatedAt:    dbUser.UpdatedAt,
+		FirstName:    dbUser.FirstName,
+		LastName:     dbUser.LastName,
+		Email:        dbUser.Email,
+		Password:     dbUser.Password,
+		ProfileImage: dbUser.ProfileImage,
 	}
 }
